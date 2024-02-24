8c75181b1a6fa166823a4f175f6d7dbbc9945c93
aneshamdaoui <bacetd2004@gmail.com>
2024-02-24 21:54:23 +0100

    preparing route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  redirect,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.tsx";
 import { Roote } from "./roote/Roote.tsx";
+import { SigninPage } from "./pages/SigninPage.tsx";
 
 const router = createBrowserRouter([
-  { path: "/your_notes", element: <Roote /> },
+  {
+    path: "/your_notes/",
+    element: <Roote />,
+    loader: () => {
+      return redirect("/your_notes/signin");
+    },
+    children: [
+      {
+        path: "signin/",
+        element: <SigninPage />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/roote/Roote.tsx
+++ src/roote/Roote.tsx
@@ -1,3 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export const Roote = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 };
